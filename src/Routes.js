7dc1118a3c79b771b1cf59cd1592be89d0cd7107
Jhonnatthan <jhonnatthans@outlook.com>
2019-11-25 16:48:32 -0300

    [ADD] - Estrutura Inicial

---
+++ src/Routes.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
+import LandingPage from './pages/LandingPage';
+import MapPage from './pages/MapPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+const Routes = props => {
+    const UnprotectedRoute = ({ component: Component, auth, ...rest }) => {
+        const { authenticated } = props;
+
+        return (
+            <Route {...rest} render={(props) => (
+                !authenticated ? <Component {...props} /> : <Redirect to='/mapa' />
+            )} />
+        )
+    }
+
+    const ProtectedRoute = ({ component: Component, auth, ...rest }) => {
+        const { authenticated } = props;
+
+        return (
+            <Route {...rest} render={(props) => (
+                authenticated ? <Component {...props} /> : <Redirect to='/' />
+            )} />
+        )
+    }
+
+    return (
+        <Router>
+            <Switch>
+                <UnprotectedRoute exact path="/" component={LandingPage} />
+                <ProtectedRoute path="/map" component={MapPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </Router>
+    );
+
+}
+
+export default Routes;
