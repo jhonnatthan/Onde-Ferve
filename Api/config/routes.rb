23f6f2c714451e513ee3359e06e98f47b9c683a7
Jhonnatthan <jhonnatthans@outlook.com>
2019-12-02 01:25:23 -0300

    [ADD] - Criação de autenticação e Model de Evento e Fotos

--- Api/config/routes.rb
+++ Api/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, param: :_username
+  resources :events, param: :_id
+  post "/auth/login", to: "authentication#login"
+  get "/*a", to: "application#not_found"
 end
