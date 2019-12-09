524029c96abdbd65640da58ff0617d11fc803bfd
Guilherme <guyi__@hotmail.com>
2019-12-08 23:42:47 -0300

    [UPDATE] corrigido cursor no menu lateral

--- Web/src/components/CardEvent.js
+++ Web/src/components/CardEvent.js
@@ -1,140 +1,144 @@
 import React, { useState, useEffect, Fragment } from "react";
 
 const styles = {
-  cardEvent: {
-    background: "#E86B52",
-    borderRadius: "8px"
-  },
+    cardEvent: {
+        background: "#E86B52",
+        borderRadius: "8px",
+        cursor: "pointer",
+    },
 
-  textData: {
-    color: "#F2F1F1",
-    fontSize: ".8rem"
-  },
-  textTitleEvent: {
-    color: "#F2F1F1",
-    fontSize: "1rem"
-  },
+    textData: {
+        color: "#F2F1F1",
+        fontSize: ".8rem",
+    },
+    textTitleEvent: {
+        color: "#F2F1F1",
+        fontSize: "1rem",
+    },
 
-  textLocal: {
-    color: "#F2F1F1",
-    fontSize: ".8rem",
-    margin: "5px 0"
-  },
+    textLocal: {
+        color: "#F2F1F1",
+        fontSize: ".8rem",
+        margin: "5px 0",
+    },
 
-  profileImg: {
-    border: "2px #E86B52 solid",
-    width: "25px",
-    height: "25px",
-    objectFit: "cover",
-    borderRadius: "17.5px",
-    backgroundColor: "white"
-  },
+    profileImg: {
+        border: "2px #E86B52 solid",
+        width: "25px",
+        height: "25px",
+        objectFit: "cover",
+        borderRadius: "17.5px",
+        backgroundColor: "white",
+    },
 
-  namesProfiles: {
-    color: "#F2F1F1",
-    fontSize: "13px"
-  },
+    namesProfiles: {
+        color: "#F2F1F1",
+        fontSize: "13px",
+    },
 
-  eventImage: {
-    width: "100%",
-    height: "90px",
-    objectFit: "cover",
-    borderRadius: "8px",
-    backgroundColor: "white"
-  }
+    eventImage: {
+        width: "100%",
+        height: "90px",
+        objectFit: "cover",
+        borderRadius: "8px",
+        backgroundColor: "white",
+    },
 };
 
 const CardEvent = props => {
-  const [event, setEvent] = useState(props.event);
+    const [event, setEvent] = useState(props.event);
 
-  const [previews, setPreviews] = useState([]);
+    const [previews, setPreviews] = useState([]);
 
-  const getEvent = () => {
-    setEvent(props.event);
-  };
+    const getEvent = () => {
+        setEvent(props.event);
+    };
 
-  const handleEvent = () => {
-    if (props.onCardClick) props.onCardClick(event);
-  };
+    const handleEvent = () => {
+        if (props.onCardClick) props.onCardClick(event);
+    };
 
-  const getPreview = () => {
-    if (event.confirmations) {
-      const firsts = event.confirmations.splice(0, 10);
+    const getPreview = () => {
+        if (event.confirmations) {
+            const firsts = event.confirmations.splice(0, 10);
 
-      const firstsName = firsts.map(first => {
-        const names = first.name.split(" ");
-        return names[0];
-      });
-      setPreviews(firstsName);
-    }
-  };
+            const firstsName = firsts.map(first => {
+                const names = first.name.split(" ");
+                return names[0];
+            });
+            setPreviews(firstsName);
+        }
+    };
 
-  const formatData = string => {
-    const dateArr = string.split("T");
+    const formatData = string => {
+        const dateArr = string.split("T");
 
-    const dayArr = dateArr[0].split("-");
+        const dayArr = dateArr[0].split("-");
 
-    const hourArr = dateArr[1].split(":");
+        const hourArr = dateArr[1].split(":");
 
-    return `${dayArr[2]}/${dayArr[1]} às ${hourArr[0]}:${hourArr[1]}`;
-  };
+        return `${dayArr[2]}/${dayArr[1]} às ${hourArr[0]}:${hourArr[1]}`;
+    };
 
-  useEffect(() => {
-    getEvent();
-  }, [props.event]);
+    useEffect(() => {
+        getEvent();
+    }, [props.event]);
 
-  useEffect(() => {
-    getEvent();
-    getPreview();
-  }, []);
+    useEffect(() => {
+        getEvent();
+        getPreview();
+    }, []);
 
-  return (
-    <div
-      style={styles.cardEvent}
-      className="row m-0 justify-content-between p-2 my-2 align-items-center"
-      onClick={handleEvent}
-    >
-      <div className="col-8">
-        <p style={styles.textData} className="m-0">
-          {formatData(event.date)}
-        </p>
-        <h4
-          style={styles.textTitleEvent}
-          className="font-weight-bold text-truncate flex-fill m-0"
+    return (
+        <div
+            style={styles.cardEvent}
+            className="row m-0 justify-content-between p-2 my-2 align-items-center"
+            onClick={handleEvent}
         >
-          {event.name}
-        </h4>
-        <span style={styles.textLocal}>{event.location}</span>
+            <div className="col-8">
+                <p style={styles.textData} className="m-0">
+                    {formatData(event.date)}
+                </p>
+                <h4
+                    style={styles.textTitleEvent}
+                    className="font-weight-bold text-truncate flex-fill m-0"
+                >
+                    {event.name}
+                </h4>
+                <span style={styles.textLocal}>{event.location}</span>
 
-        <div className="d-flex flex-row">
-          {previews.map((preview, idx) => (
+                <div className="d-flex flex-row">
+                    {previews.map((preview, idx) => (
+                        <img
+                            key={idx}
+                            className="event__profile"
+                            src="/assets/images/user.png"
+                            style={styles.profileImg}
+                            alt=""
+                        />
+                    ))}
+                </div>
+
+                {previews.length > 0 && (
+                    <Fragment>
+                        <p
+                            style={styles.namesProfiles}
+                            className="m-0 text-truncate"
+                        >
+                            {previews.join(", ")}
+                        </p>
+                    </Fragment>
+                )}
+            </div>
             <img
-              key={idx}
-              className="event__profile"
-              src="/assets/images/user.png"
-              style={styles.profileImg}
-              alt=""
+                className="col-4 p-0"
+                style={styles.eventImage}
+                src={event.banner}
+                alt=""
+                srcSet=""
             />
-          ))}
         </div>
-
-        {previews.length > 0 && (
-          <Fragment>
-            <p style={styles.namesProfiles} className="m-0 text-truncate">
-              {previews.join(", ")}
-            </p>
-          </Fragment>
-        )}
-      </div>
-      <img
-        className="col-4 p-0"
-        style={styles.eventImage}
-        src={event.banner}
-        alt=""
-        srcSet=""
-      />
-    </div>
-  );
+    );
 };
 
 export default CardEvent;
--- Web/src/pages/MapPage.js
+++ Web/src/pages/MapPage.js
@@ -7,152 +7,166 @@ import storage from "../services/storage";
 import api from "../services/api";
 
 const styles = {
-  mapContainer: {
-    maxHeight: "100vh",
-    minHeight: "100vh"
-  },
-  textEvent: {
-    color: "#E86B52"
-  },
-  btnGroupStyle: {
-    background: "#fff",
-    border: "2px #E86B52 solid",
-    color: "#E86B52"
-  },
+    mapContainer: {
+        maxHeight: "100vh",
+        minHeight: "100vh",
+    },
+    textEvent: {
+        color: "#E86B52",
+    },
+    btnGroupStyle: {
+        background: "#fff",
+        border: "2px #E86B52 solid",
+        color: "#E86B52",
+    },
 
-  btnGroupStyleActive: {
-    background: "#E86B52",
-    color: "#fff"
-  },
+    btnGroupStyleActive: {
+        background: "#E86B52",
+        color: "#fff",
+    },
 
-  textLabel: {
-    color: "#E86B52",
-    fontSize: "1.5rem"
-  },
+    textLabel: {
+        color: "#E86B52",
+        fontSize: "1.5rem",
+    },
 
-  textError: {
-    color: "#E86B52",
-    letterSpacing: 0,
-    opacity: 1
-  },
+    textError: {
+        color: "#E86B52",
+        letterSpacing: 0,
+        opacity: 1,
+    },
 
-  footerSideMenu: {
-    background: "#E86B52",
-    borderRadius: "10px 10px 0 0"
-  },
+    footerSideMenu: {
+        background: "#E86B52",
+        borderRadius: "10px 10px 0 0",
+    },
 
-  footerSideMenuImg: {
-    border: "2px #E86B52 solid",
-    width: "30px",
-    height: "30px",
-    borderRadius: "15px",
-    objectFit: "contain",
-    backgroundColor: "white"
-  },
+    footerSideMenuImg: {
+        border: "2px #E86B52 solid",
+        width: "30px",
+        height: "30px",
+        borderRadius: "15px",
+        objectFit: "contain",
+        backgroundColor: "white",
+    },
 
-  footerSideMenuName: {
-    color: "#F2F1F1",
-    fontSize: ".9rem"
-  },
-  rightContainer: {
-    maxHeight: "100vh"
-  },
-  barContainer: {
-    flex: 1,
-    overflow: "scroll"
-  }
+    footerSideMenuName: {
+        color: "#F2F1F1",
+        fontSize: ".9rem",
+    },
+    rightContainer: {
+        maxHeight: "100vh",
+    },
+    barContainer: {
+        flex: 1,
+        overflow: "scroll",
+    },
+    iconLogout: {
+        cursor: "pointer",
+    },
 };
 
 const MapPage = ({ history: { push } }) => {
-  const [user, setUser] = useState(null);
-  const [events, setEvents] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-  const [event, setEvent] = useState(null);
+    const [user, setUser] = useState(null);
+    const [events, setEvents] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [event, setEvent] = useState(null);
 
-  const logout = () => {
-    storage.clear();
-    push("/");
-  };
+    const logout = () => {
+        storage.clear();
+        push("/");
+    };
 
-  const getEvents = async () => {
-    try {
-      const response = await api.get("/events");
-      const { data } = response;
+    const getEvents = async () => {
+        try {
+            const response = await api.get("/events");
+            const { data } = response;
 
-      if (!data.error) {
-        setEvents(data.data);
-      } else {
-        alert(data.message);
-      }
-    } catch (error) {
-      alert(error);
-    }
-  };
+            if (!data.error) {
+                setEvents(data.data);
+            } else {
+                alert(data.message);
+            }
+        } catch (error) {
+            alert(error);
+        }
+    };
 
-  const getUser = () => {
-    const _user = storage.getUser();
-    setUser(_user);
-  };
+    const getUser = () => {
+        const _user = storage.getUser();
+        setUser(_user);
+    };
 
-  useEffect(() => {
-    getUser();
-    getEvents();
-  }, []);
+    useEffect(() => {
+        getUser();
+        getEvents();
+    }, []);
 
-  const eventOpen = _event => {
-    setEvent(_event);
-    setShowModal(true);
-  };
+    const eventOpen = _event => {
+        setEvent(_event);
+        setShowModal(true);
+    };
 
-  return (
-    <main>
-      <div className="row m-0" style={styles.mapContainer}>
-        <div className="col-12 col-sm-9 border justify-content-center align-items-start d-flex map-container p-0">
-          <MapContainer markerClick={eventOpen} events={events} />
-          {/* APARECER VIA MODAL/ANIMAÇÃO */}
-          {showModal && (
-            <ModalEvent onClose={() => setShowModal(false)} event={event} />
-          )}
-        </div>
-        <div
-          className="col-12 col-sm-3 justify-content-between d-flex flex-column"
-          style={styles.rightContainer}
-        >
-          <div className="d-flex my-2 flex-column" style={styles.barContainer}>
-            <h3 className="text-center" style={styles.textEvent}>
-              Eventos
-            </h3>
-            {events.map(event => (
-              <CardEvent key={event.id} event={event} onCardClick={eventOpen} />
-            ))}
-          </div>
+    return (
+        <main>
+            <div className="row m-0" style={styles.mapContainer}>
+                <div className="col-12 col-sm-9 border justify-content-center align-items-start d-flex map-container p-0">
+                    <MapContainer markerClick={eventOpen} events={events} />
+                    {/* APARECER VIA MODAL/ANIMAÇÃO */}
+                    {showModal && (
+                        <ModalEvent
+                            onClose={() => setShowModal(false)}
+                            event={event}
+                        />
+                    )}
+                </div>
+                <div
+                    className="col-12 col-sm-3 justify-content-between d-flex flex-column"
+                    style={styles.rightContainer}
+                >
+                    <div
+                        className="d-flex my-2 flex-column"
+                        style={styles.barContainer}
+                    >
+                        <h3 className="text-center" style={styles.textEvent}>
+                            Eventos
+                        </h3>
+                        {events.map(event => (
+                            <CardEvent
+                                key={event.id}
+                                event={event}
+                                onCardClick={eventOpen}
+                            />
+                        ))}
+                    </div>
 
-          <div
-            className="px-4 py-2 d-flex align-items-center justify-content-center"
-            style={styles.footerSideMenu}
-          >
-            <img
-              src="/assets/images/user.png"
-              style={styles.footerSideMenuImg}
-              alt=""
-              className="mr-2"
-            />
-            <p
-              style={styles.footerSideMenuName}
-              className="m-0 font-weight-bold d-flex flex-fill text-truncate"
-            >
-              {user && user.name}
-            </p>
-            <i
-              onClick={logout}
-              className="fa fa-sign-out text-white d-flex ml-auto h5 m-0"
-              aria-hidden="true"
-            ></i>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+                    <div
+                        className="px-4 py-2 d-flex align-items-center justify-content-center"
+                        style={styles.footerSideMenu}
+                    >
+                        <img
+                            src="/assets/images/user.png"
+                            style={styles.footerSideMenuImg}
+                            alt=""
+                            className="mr-2"
+                        />
+                        <p
+                            style={styles.footerSideMenuName}
+                            className="m-0 font-weight-bold d-flex flex-fill text-truncate"
+                        >
+                            {user && user.name}
+                        </p>
+                        <i
+                            onClick={logout}
+                            style={styles.iconLogout}
+                            className="fa fa-sign-out text-white d-flex ml-auto h5 m-0"
+                            aria-hidden="true"
+                        ></i>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default MapPage;
